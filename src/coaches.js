1289b0c587c19dd7a41481ec612a4f2e41a017d4
M Balvert <mikebalvert@icloud.com>
2024-05-31 10:40:23 +0200

    goes trouth coach cards if for the page it will show  (#11)

--- src/coaches.js
+++ src/coaches.js
@@ -1,3 +1,21 @@
+function getQueryParams() {
+    let params = {};
+    let queryString = window.location.search.slice(1);
+    let queryArray = queryString.split('&');
+    queryArray.forEach(function(param) {
+        let [key, value] = param.split('=');
+        params[key] = decodeURIComponent(value);
+    });
+    return params;
+}
+
+let params = getQueryParams();
+if (params.specialty) {
+    console.log(params.specialty);
+} else {
+    console.log("No specialty provided");
+}
+
 coachesDiv = document.getElementById("coaches");
 async function getData() {
     try {
@@ -15,14 +33,15 @@ async function getData() {
 const promise = getData()
 const template = document.getElementById("coach");
 promise.then(data => {
-    console.log(data.length)
     for (let i = 0; i < data.length; i++) {
-        let cartDiv = document.createElement("div");
-        cartDiv.classList.add("card");
-        const cartDivName = template.content.cloneNode(true);
-        cartDivName.querySelector("#naam").textContent = data[i].name;
-        cartDivName.querySelector("#image").src = data[i].image;
-        cartDivName.querySelector("#specialty").textContent = data[i].specialty;
-        document.body.appendChild(cartDivName);
+        if (params.specialty == data[i].specialty) {
+            let cartDiv = document.createElement("div");
+            cartDiv.classList.add("card");
+            const cartDivName = template.content.cloneNode(true);
+            cartDivName.querySelector("#naam").textContent = data[i].name;
+            cartDivName.querySelector("#image").src = data[i].image;
+            cartDivName.querySelector("#specialty").textContent = data[i].specialty;
+            document.body.appendChild(cartDivName);
+        }
     }
 })
