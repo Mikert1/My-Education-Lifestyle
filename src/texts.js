b25217ee77f21a99fb4cb33577af30e980ce796e
M Balvert <mikebalvert@icloud.com>
2024-06-05 10:45:43 +0200

    made a texts.json file so every website text can be edited

---
+++ src/texts.js
@@ -0,0 +1,32 @@
+async function getData() {
+    try {
+        const response = await fetch('src/texts.json');
+        if (!response.ok) {
+            throw new Error('Failed to fetch');
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching:', error);
+        throw error;
+    }
+}
+
+getData()
+    .then(data => {
+        let element;
+        for (let i = 0; i <= 3; i++) {
+            if (document.getElementById(i)) {
+                element = document.getElementById(i);
+                for (let j = 0; j < data.length; j++) {
+                    if (element.id == data[j].id) {
+                        element.textContent = data[j].text;
+                    }
+                }
+            }
+        }
+    })
+    .catch(error => {
+        console.error('Error fetching:', error);
+        throw error;
+    });
