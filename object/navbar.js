13ee1f422a943a2c537666f9ab28f3cef78d22c7
Mika Stengewis <mika@stengewis.nl>
2024-06-07 11:25:13 +0200

    navbar nu echt  af #3

--- object/navbar.js
+++ object/navbar.js
@@ -1,10 +1,12 @@
 const menuIcon = document.getElementById('menu-icon');
 const navList = document.getElementById('nav-list');
+const navbarObject = document.getElementById('navbarObject');
 
-// Toggle nav list display on menu icon click
+
 menuIcon.addEventListener('click', function () {
     navList.classList.toggle('active');
     menuIcon.classList.toggle('rotate');
+    window.parent.document.getElementById('navbarObject').classList.toggle('active');
 });
 
 
