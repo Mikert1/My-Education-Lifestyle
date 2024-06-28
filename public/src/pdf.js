d9bc15ac0adf8c54c23545062ed31027d3a8a1b7
Fabio Salvador <fabiosalvador2906@outlook.com>
2024-06-28 14:58:47 +0200

    pdf pages changable and only go to the max pages or the minimum

--- public/src/pdf.js
+++ public/src/pdf.js
@@ -1,15 +1,12 @@
-document.addEventListener('DOMContentLoaded', function () {
+let pageNumber = 1;
+let PageNumbers = null;
+function load() {
     const url = 'pdf/Document.pdf'; // Update with the path to your PDF
 
     const loadingTask = pdfjsLib.getDocument(url);
     loadingTask.promise.then(function (pdf) {
-        console.log('PDF loaded');
-
-        const pageNumber = 1;
         pdf.getPage(pageNumber).then(function (page) {
-            console.log('Page loaded');
-
-            const scale = 1.5;
+            const scale = 0.9;
             const viewport = page.getViewport({ scale: scale });
 
             const canvas = document.createElement('canvas');
@@ -25,8 +22,33 @@ document.addEventListener('DOMContentLoaded', function () {
             };
             const renderTask = page.render(renderContext);
             renderTask.promise.then(function () {
-                console.log('Page rendered');
             });
         });
+        const totalPage = document.getElementById('TotalPages');
+        const page = document.getElementById('PageNumber');
+        totalPage.innerHTML = pdf.numPages;
+        page.innerHTML = pageNumber;
+        PageNumbers = pdf.numPages;
+        return PageNumbers;
     });
+}
+document.addEventListener('DOMContentLoaded', function () {
+    load();
 });
+function NextPage(id) {
+    if (id == "next" && pageNumber < PageNumbers) {
+        pageNumber += 1;
+        const pdfViewer = document.getElementById('pdfViewer');
+        while (pdfViewer.firstChild) {
+            pdfViewer.removeChild(pdfViewer.firstChild);
+        }
+        load();
+    } else if (id == "prev" && pageNumber > 1) {
+        pageNumber -= 1;
+        const pdfViewer = document.getElementById('pdfViewer');
+        while (pdfViewer.firstChild) {
+            pdfViewer.removeChild(pdfViewer.firstChild);
+        }
+        load();
+    }
+}
